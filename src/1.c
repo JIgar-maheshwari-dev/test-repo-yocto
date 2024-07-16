35c38ee63113bf977388750dd3610bb125ebfd5f
JIgar-maheshwari-dev <83265810+JIgar-maheshwari-dev@users.noreply.github.com>
2024-07-16 17:53:17 +0530

    Update 1.c

--- src/1.c
+++ src/1.c
@@ -3,5 +3,9 @@
 int main(void){
 	printf("VAL = %d \n",VAL );
 	printf("Hello file .... \n");
+	printf("Hello file 1.... \n");
+	printf("Hello file .2... \n");
+	printf("Hello file ..3.. \n");
+	
 	return 0;
 }
